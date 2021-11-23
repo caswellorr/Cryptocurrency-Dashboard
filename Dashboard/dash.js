bc7aa8846f6f6e577283b61ed1a008bde9a7354f
Caswell Orr <caswell.orr@gmail.com>
2021-11-23 13:18:37 -0600

    switching gears. sending it with the graph. early stages...

--- Dashboard/dash.js
+++ Dashboard/dash.js
@@ -18,58 +18,34 @@
 
 // function getApi () {
 
-//   let requestUrl = "https://poloniex.com/public?command=returnCurrencies";
+  let requestUrl = "https://poloniex.com/public?command=returnCurrencies";
+  requestUrl = `https://poloniex.com/public?command=returnChartData&currencyPair=USDT_${"BTC"}&start=1546300800&end=1546646400&period=14400`
 
-//   fetch (requestUrl)
-//   .then (function (response){
-//     return response.json();
-//   })
-//   .then(function (currency){
-//     console.log(currency);
+  fetch (requestUrl)
+  .then (function (response){
+    return response.json();
+  })
+  .then(function (currency){
+    console.log(currency);
 
-//   })
+  })
 // }
 
 // getApi()
 
-// ========= JUMBOTRON CHART ==========
-const dataSource = [{
-  date: new Date(1994, 2, 1),
-  l: 24.00,
+// // ========= JUMBOTRON CHART ==========
+// potentially for looping throught the data your getting back and inserting the data per element in the
+//for 
+let lval = currency[i].low
+let datespan = currency[i].date
+let object = {date: new Date(datespanyear, datespanmonth, 1),
+  l: lval,
   h: 25.00,
   o: 25.00,
-  c: 24.875,
-}, {
-  date: new Date(1994, 2, 2),
-  l: 23.625,
-  h: 25.125,
-  o: 24.00,
-  c: 24.875,
-}, {
-  date: new Date(1994, 2, 3),
-  l: 26.25,
-  h: 28.25,
-  o: 26.75,
-  c: 27.00,
-}, {
-  date: new Date(1994, 2, 4),
-  l: 26.50,
-  h: 27.875,
-  o: 26.875,
-  c: 27.25,
-}, {
-  date: new Date(1994, 2, 7),
-  l: 26.375,
-  h: 27.50,
-  o: 27.375,
-  c: 26.75,
-}, {
-  date: new Date(1994, 2, 8),
-  l: 25.75,
-  h: 26.875,
-  o: 26.75,
-  c: 26.00,
-},];
+  c: 24.875,}
+  dataSource.push(object)
+
+const dataSource = [];
 
 $(() => {
   $('#chart').dxChart({
@@ -86,10 +62,10 @@ $(() => {
       {
           // name : comes from input in searchbtn
         name: 'DELL',
-        openValueField: 'open',
-        highValueField: 'high',
-        lowValueField: 'low',
-        closeValueField: 'close',
+        openValueField: 'o',
+        highValueField: 'h',
+        lowValueField: 'l',
+        closeValueField: 'c',
         reduction: {
           color: 'red',
         },
